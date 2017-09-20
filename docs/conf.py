fe574a7e82dbf8c7754a558b1c7ed7d9fc2a481c
vegacdn <32118865+vegacdn@users.noreply.github.com>
2017-09-20 12:31:35 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,8 +43,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Read the Docs Template'
-copyright = u'2014, Read the Docs'
+project = u'VegaCDN API'
+copyright = u'2017, VegaCDN API'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -196,8 +196,8 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'ReadtheDocsTemplate.tex', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'manual'),
+  ('index', 'ReadtheDocsTemplate.tex', u'VegaCDN API Documentation',
+   u'VegaCDN API', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -226,8 +226,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'readthedocstemplate', u'Read the Docs Template Documentation',
-     [u'Read the Docs'], 1)
+    ('index', 'readthedocstemplate', u'VegaCDN API Documentation',
+     [u'VegaCDN API'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -240,8 +240,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'ReadtheDocsTemplate', u'Read the Docs Template Documentation',
-   u'Read the Docs', 'ReadtheDocsTemplate', 'One line description of project.',
+  ('index', 'ReadtheDocsTemplate', u'VegaCDN API Documentation',
+   u'VegaCDN API', 'ReadtheDocsTemplate', 'One line description of project.',
    'Miscellaneous'),
 ]
 
